50cf2ea589ba00bbd3388cd85ff32db56c2ad836
Erik Ytterman <erik.ytterman@phaseshift.se>
2016-03-23 07:07:51 -0700

    Sync...

--- scripts/compile_stations_mr.sh
+++ scripts/compile_stations_mr.sh
@@ -1,6 +1,6 @@
 export HADOOP_CLASSPATH=$JAVA_HOME/lib/tools.jar
 rm -Rfv ./build/*
-hadoop com.sun.tools.javac.Main  -sourcepath source -d build source/**/*.java
+hadoop com.sun.tools.javac.Main -sourcepath source -d build source/**/*.java
 jar cf  JsonlStationsETL.jar -C build .
 echo "Listing contents of JsonlStationsETL.jar"
 jar tvf JsonlStationsETL.jar
